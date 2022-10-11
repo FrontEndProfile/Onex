07f1e2c234a46212bbf6de44ed5ef064b739711e
VigoFazal <frontendprofile@gmail.com>
2022-10-11 03:31:39 -0700

    v-07

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
 $(document).ready(function () {
     $('#example').DataTable();
 });
+
+
+$( ".detail-btn" ).click(function() {
+    $(this).prev( ".farma-hidden" ).slideToggle();
+    $(this).toggleClass("active");
+    $(this).prev( ".farma-hidden" ).prev(".unlock-btn").toggleClass("active");
+  });
+
+  $( ".menu-icon" ).click(function() { 
+    $(".nav").addClass("active"); 
+  });
+
+  $( ".nav-close" ).click(function() { 
+    $(".nav").removeClass("active"); 
+  });
